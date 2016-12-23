e4ea07009b5587b438d9c530aee8717004fbc3da
krishna janoliya <krishna.janolia@gmail.com>
2016-12-23 12:49:08 +0530

    Create NewEnum.java

---
+++ enum/basic_example/NewEnum.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package krishna;
+
+/**
+ *
+ * @author student
+ */
+public class enumclass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewEnum n1;
+        NewEnum n3;
+        n1=NewEnum.krishna;
+        NewEnum n2;
+        n2=NewEnum.krishna;
+        n3=NewEnum.anjali;
+        System.out.println(n3);
+    }
+}
+

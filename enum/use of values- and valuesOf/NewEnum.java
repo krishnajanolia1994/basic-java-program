0c531c6ec4e9ef34c335cf7245c6ee177a6434b2
krishna janoliya <krishna.janolia@gmail.com>
2016-12-23 13:38:24 +0530

    Create NewEnum.java

---
+++ enum/use of values- and valuesOf/NewEnum.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package krishna;
+
+/**
+ *
+ * @author student
+ */
+public enum NewEnum {
+    nikhil,anjali,krishna,joy;
+    
+}

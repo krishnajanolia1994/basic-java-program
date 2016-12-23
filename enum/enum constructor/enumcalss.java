5f24da2e7974e9c44345f1d0f135b4ed6f1b45f9
krishna janoliya <krishna.janolia@gmail.com>
2016-12-23 13:56:51 +0530

    Create enumcalss.java

---
+++ enum/enum constructor/enumcalss.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package krishna;
+
+/**
+ *
+ * @author student
+ */
+public class enumclass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewEnum n2;
+        NewEnum n3;
+        n2=NewEnum.anjali;
+        n3=NewEnum.krishna;
+        System.out.println("value of "+ n2+"is "+n2.getPrise() );
+        System.out.println("value of "+ n3+"is "+n3.getPrise() );
+    }
+}

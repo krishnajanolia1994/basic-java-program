c4fb197e5beedb48088c08039d997ede6c287850
krishna janoliya <krishna.janolia@gmail.com>
2016-12-24 13:12:36 +0530

    Create geverics.java

---
+++ generics/A Generic Class with Two Type Parameters/geverics.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package krishna.generics;
+
+/**
+ *
+ * @author student
+ */
+public class generics<t,v> {
+    t ob;
+    v ob2;
+    generics (t a,v b)
+    {
+        ob=a;
+        ob2=b;
+    }
+    public t getob1()
+    {
+        return ob;
+    }
+    public v getob2()
+    {
+        return ob2;
+    }
+    public void sowtype()
+    {
+        System.out.println("the perameter of object ob is  " + ob.getClass().getName());
+        System.out.println("the perameter of object ob2 is  " + ob2.getClass().getName());
+
+    }
+}

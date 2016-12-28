94c0d40be4d36a6c5ea06c3bf86af207ea3b449d
krishna janoliya <krishna.janolia@gmail.com>
2016-12-28 17:56:32 +0530

    Create comp.java

---
+++ java.util/comp.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author student
+ */import java.util.*;
+ 
+public class comp implements Comparator<String> {
+    
+   public int compare(String s,String s1)
+   {
+       return s1.compareTo(s);
+   }
+    
+}

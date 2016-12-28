5c5c9f48be7839fcc1fbfba470ff56533d9c61d5
krishna janoliya <krishna.janolia@gmail.com>
2016-12-28 12:59:56 +0530

    Create treeset.java

---
+++ java.util/treeset.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newkrishna.java_util;
+
+import java.util.*;
+
+
+/**
+ *
+ * @author student
+ */
+public class treeset {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TreeSet hs=new TreeSet();
+        Integer i=new Integer(12);
+        hs.add(i);
+        i=15;
+        hs.add(i);
+        i=19;
+        hs.add(i);
+        Iterator it=hs.iterator();
+        while(it.hasNext())
+        {
+            System.out.println(it.next());
+        }
+    }
+    
+}

09b3b699eacc79719f655855ceffe9413133071d
krishna janoliya <krishna.janolia@gmail.com>
2016-12-28 11:14:33 +0530

    Create ArryList1.java

---
+++ java.util/ArryList1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newkrishna.java_util;
+import java.util.*;
+
+/**
+ *
+ * @author student
+ */
+public class ArrayList1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        Integer i=new Integer(10);
+        l.add(i);
+        l.add(i);
+        l.add(i);
+        i=20;
+        l.add(1, i);
+        l.remove(1);
+        Iterator it=l.iterator();
+        while(it.hasNext())
+        {
+            System.out.println(it.next());
+        }
+    }
+    
+}

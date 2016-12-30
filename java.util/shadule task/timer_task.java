8a5955ef5ffe20c78452080990dcbee1db2b3c8f
krishna janoliya <krishna.janolia@gmail.com>
2016-12-30 13:09:40 +0530

    Create timer_task.java

---
+++ java.util/shadule task/timer_task.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newkrishna.java_util;
+
+/**
+ *
+ * @author student
+ */
+import java.util.*;
+public class timer_task extends TimerTask {
+    public void run()
+    {
+        System.out.println("timer task is called");
+    }
+    
+}

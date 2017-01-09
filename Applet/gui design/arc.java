c946e1f1de90968b102d365580bec573bd9f61bf
krishna janoliya <krishna.janolia@gmail.com>
2017-01-09 16:48:37 +0530

    Create arc.java

---
+++ Applet/gui design/arc.java
@@ -0,0 +1,17 @@
+
+package networkin_in_java;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class arc extends Applet {
+
+    
+    public void init() {
+    }
+    public void paint(Graphics g)
+    {
+        g.drawArc(0, 0, 100,200, 90,180);
+    }
+
+}

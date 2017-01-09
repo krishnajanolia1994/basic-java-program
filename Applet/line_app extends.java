0bee63c1138707cd88db44d112f5d2c5df78f46b
krishna janoliya <krishna.janolia@gmail.com>
2017-01-09 16:36:34 +0530

    Create line_app extends.java

---
+++ Applet/line_app extends.java
@@ -0,0 +1,17 @@
+
+package networkin_in_java;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class line_app extends Applet {
+
+    
+    public void init() {
+    }
+    public void paint(Graphics g)
+    {
+        g.drawLine(100, 200,250, 350);
+    }
+
+}

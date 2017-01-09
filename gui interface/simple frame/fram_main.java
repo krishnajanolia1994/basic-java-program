01309a65b1a1070526357f9a1e40131966d0d9e9
krishna janoliya <krishna.janolia@gmail.com>
2017-01-09 16:11:41 +0530

    Create fram_main.java

---
+++ gui interface/simple frame/fram_main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package networkin_in_java;
+
+/**
+ *
+ * @author student
+ */
+public class fram_main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewClass n= new NewClass();
+        String str="krishna";
+        n.setSize(200,200);
+        n.setVisible(true);
+        n.setTitle(str);
+    }
+    
+}

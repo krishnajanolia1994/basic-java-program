795f7d5904dadf1301ddc4657d9358ffc722673c
krishna janoliya <krishna.janolia@gmail.com>
2017-01-09 16:39:07 +0530

    Create line_app.java

---
+++ Applet/line_app.java
@@ -0,0 +1,17 @@
+
+package networkin_in_java;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class line_app extends Applet {
+
+    
+    public void init() {
+    }
+    public void paint(Graphics g)
+    {
+        g.drawRect(0,20 ,100,120);
+    }
+
+}

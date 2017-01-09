10ff5d2a85943393f0819a99a3d8fdae7238374f
krishna janoliya <krishna.janolia@gmail.com>
2017-01-09 16:43:40 +0530

    Create oval.java

---
+++ Applet/gui design/oval.java
@@ -0,0 +1,17 @@
+
+package networkin_in_java;
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class oval extends Applet {
+
+    
+    public void init() {
+    }
+    public void paint(Graphics g)
+    {
+        g.drawOval(0,0, 100,150);
+    }
+
+}

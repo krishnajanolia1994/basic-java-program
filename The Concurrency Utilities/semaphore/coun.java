2b0ef33a4b3e2e386dfe5ec5b173405cb716a0bc
krishna janoliya <krishna.janolia@gmail.com>
2017-01-13 15:12:51 +0530

    Create coun.java

---
+++ The Concurrency Utilities/semaphore/coun.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author student
+ */
+public class coun {
+    coun()
+    {}
+    static int count=0;
+}

43004fd9ae997016235654aab784cf932a110c17
krishna janoliya <krishna.janolia@gmail.com>
2017-01-16 15:33:12 +0530

    Create Div.java

---
+++ The Concurrency Utilities/thread executor/callable and future inyrterface/Div.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+import java.util.concurrent.Callable;
+
+/**
+ *
+ * @author student
+ */
+class Div implements Callable<Integer> {
+
+    int i,j;
+    public Div(int i, int i0) {
+        this.i=i;
+        j=i0;
+    }
+    public Integer call()
+    
+    {
+        return i/j;
+    }
+    
+}

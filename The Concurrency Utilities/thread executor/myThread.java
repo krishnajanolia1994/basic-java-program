44f953f7fafae7631ff5d23b65e6f7382f94c6e5
krishna janoliya <krishna.janolia@gmail.com>
2017-01-16 14:16:15 +0530

    Create myThread.java

---
+++ The Concurrency Utilities/thread executor/myThread.java
@@ -0,0 +1,30 @@
+
+package newpackage;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ *
+ * @author student
+ */
+class myThread implements Runnable{
+    CountDownLatch cdl;
+    String s;
+
+    public myThread(CountDownLatch cdl,String s) {
+        this.cdl=cdl;
+        this.s=s;
+        new Thread( this).start();
+    }
+    public void run()
+    {
+        int i;
+        for(i=0;i<10;i++)
+        {
+            cdl.countDown();
+            System.out.println(i+ " in thread  "+s);
+            
+        }
+    }
+    
+}

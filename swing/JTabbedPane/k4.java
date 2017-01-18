e5db9d29b8257fdf9e68e374bdb0a70226df0659
krishna janoliya <krishna.janolia@gmail.com>
2017-01-18 18:49:03 +0530

    Create k4.java

---
+++ swing/JTabbedPane/k4.java
@@ -0,0 +1,24 @@
+
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author student
+ */
+class k4 extends JPanel {
+    JButton jb;
+    public k4() {
+        jb=new JButton("khushabu");
+        add(jb);
+
+    }
+    
+}

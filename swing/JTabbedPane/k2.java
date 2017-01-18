ac3047b60e0055dc919e5678b0c201234793e0bf
krishna janoliya <krishna.janolia@gmail.com>
2017-01-18 18:49:32 +0530

    Create k2.java

---
+++ swing/JTabbedPane/k2.java
@@ -0,0 +1,24 @@
+
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author student
+ */
+class k2 extends JPanel {
+    JButton jb;
+    public k2() {
+        jb=new JButton("anjali");
+        add(jb);
+
+    }
+    
+}

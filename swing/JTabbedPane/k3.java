c10408c553d84997e6eb84dace36ee5e78d8dcd6
krishna janoliya <krishna.janolia@gmail.com>
2017-01-18 18:44:11 +0530

    Create k3.java

---
+++ swing/JTabbedPane/k3.java
@@ -0,0 +1,24 @@
+
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author student
+ */
+class k3 extends JPanel {
+    JButton jb;
+    public k3() {
+        jb=new JButton("devesh");
+        add(jb);
+
+    }
+    
+}

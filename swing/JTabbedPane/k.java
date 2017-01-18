b49366446faae7cd2ee6bc175fd3740a3011d14b
krishna janoliya <krishna.janolia@gmail.com>
2017-01-18 18:43:08 +0530

    Create k.java

---
+++ swing/JTabbedPane/k.java
@@ -0,0 +1,23 @@
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author student
+ */
+class k extends JPanel{
+
+    JButton jb;
+    public k() {
+        jb=new JButton("krishna");
+        add(jb);
+    }
+    
+}
